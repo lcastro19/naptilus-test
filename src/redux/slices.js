35307a8a81a41b9b8216b26cb5151b3d9ff0a65a
Lazaro Castro <lazarojcastro1990@gmail.com>
2024-01-28 22:01:56 +0100

    refactor slice

--- src/redux/slices.js
+++ src/redux/slices.js
@@ -1,32 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const updateArray = (state, data, key) => {
-    const index = state.findIndex(val => val[key] === data[key])
+    const index = state.findIndex((val) => val[key] === data[key]);
+
     if (index !== -1) {
-        state[index] = data
-    } else state.push(data)
-}
+        state[index] = data;
+    } else {
+        state.push(data);
+    }
+};
 
-const listSlice = createSlice({
-    name: 'list',
-    initialState: [],
-    reducers: {
-        setList: (state, action) => updateArray(state, action.payload, 'current')
-    },
-});
+const createListSlice = (name, key) => {
+    return createSlice({
+        name,
+        initialState: [],
+        reducers: {
+            setItem: (state, action) => updateArray(state, action.payload, key),
+        },
+    });
+};
 
+export const listSlice = createListSlice('list', 'current');
+export const itemSlice = createListSlice('item', 'id');
 
-const itemSlice = createSlice({
-    name: 'item',
-    initialState: [],
-    reducers: {
-        setItem: (state, action) => updateArray(state, action.payload, 'id')
-    },
-});
-
-export const { setList } = listSlice.actions;
+export const { setItem: setList } = listSlice.actions;
 export const { setItem } = itemSlice.actions;
 
 export const listReducer = listSlice.reducer;
 export const itemReducer = itemSlice.reducer;
-
